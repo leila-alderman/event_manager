b8e540212be69f31eb0b4d697fd6596498307ebd
leila-alderman <leila.alderman@gmail.com>
2019-01-21 18:10:03 -0500

    Refactor clean_zipcode method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,19 +1,7 @@
 require 'csv'
 
 def clean_zipcode(zipcode)
-    # If the zipcode is missing, add in the placeholder missing code of "00000".
-    if zipcode.nil?
-        zipcode = "00000"
-    # If the zipcode is shorter than five digits, add zeros to the front. (Assume New England address.)
-    elsif zipcode.length < 5
-        zipcode = zipcode.rjust(5,"0")
-    # If the zipcode is longer than five digits, truncate to the first five digits.
-    elsif zipcode.length > 5
-        zipcode = zipcode[0..4]
-    # If the zipcode is five digits long, assume that it's fine.
-    else
-        zipcode
-    end
+    zipcode.to_s.rjust(5,"0")[0..4]
 end
 
 
