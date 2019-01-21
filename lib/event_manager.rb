56d7e6bb10fabcb466f2892534ffd1bdba92af91
leila-alderman <leila.alderman@gmail.com>
2019-01-21 18:47:21 -0500

    Add method for saving thank you notes

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
 
 def clean_zipcode(zipcode)
     zipcode.to_s.rjust(5,"0")[0..4]
@@ -13,23 +14,31 @@ def legislators_by_zipcode(zipcode)
             address: zipcode,
             levels: 'country',
             roles: ['legislatorUpperBody', 'legislatorLowerBody']
-        )
-        legislators = legislators.officials
-        legislator_names = legislators.map(&:name)
-        legislators_string = legislator_names.join(", ")
+        ).officials
     rescue
         "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
     end
 end
 
+def save_thank_you_letters(id, form_letter)
+    Dir.mkdir("output") unless Dir.exists? "output"
+    filename = "output/thanks_#{id}.html"
+    File.open(filename, 'w') do |file|
+        file.put form_letter
+    end
+end
 
 puts "Event Manager Initialized!"
 
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+template_letter = File.read "form_letter.erb"
+erb_template = ERB.new template_letter
+
 contents.each do |row|
+    id = row[0]
     name = row[:first_name]
     zipcode = clean_zipcode(row[:zipcode])
     legislators = legislators_by_zipcode(zipcode)
-
-    puts "#{name} #{zipcode} #{legislators}"
+    form_letter = erb_template.result(binding)
+    save_thank_you_letters(id, form_letter)
 end
